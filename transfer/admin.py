6aec56e34fc0e11bd09c467469253588857677c7
cmeehan340 <c.meehan340@gmail.com>
2020-04-04 10:14:43 -0400

    Implemented MajorRequirments model, and added unique attribute to other fields

--- transfer/admin.py
+++ transfer/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models.models_transfer import School, Course
+from .models.models_transfer import School, Course, MajorRequirment
 
 # Register your models here.
 admin.site.register(School)
 admin.site.register(Course)
+admin.site.register(MajorRequirment)
--- transfer/models/models_transfer.py
+++ transfer/models/models_transfer.py
@@ -3,15 +3,18 @@ from django.db import models
 # Create your models here.
 
 class School(models.Model):
-    school_name = models.CharField(max_length=100)
+    school_name = models.CharField(max_length=100, unique=True)
 
 
 class Course(models.Model):
     school_id = models.ForeignKey(School, on_delete=models.CASCADE)
-    subject_no = models.CharField(max_length = 10)
+    subject_no = models.CharField(max_length = 10, unique=True)
     title = models.CharField(max_length=100)
     sem_year_taken = models.CharField(max_length = 8)
     expiration_date = models.DateField()
     approved_status = models.CharField(max_length=1)
     comment = models.CharField(max_length = 150)
     # approver_id = models.ForeignKey(Approver, on_delete=models.CASCADE)
+
+class MajorRequirment(models.Model):
+    description = models.CharField(max_length = 100, unique=True)
