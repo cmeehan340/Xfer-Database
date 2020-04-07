85e0575975e4243d21225ea31d8904fdb70b1d4f
cmeehan340 <c.meehan340@gmail.com>
2020-04-07 09:00:35 -0400

    Tied the code that we had together more, linked pages, still need to actually build dropdowns

--- transfer/views/views.py
+++ transfer/views/views.py
@@ -4,9 +4,17 @@ from django.http import HttpResponse
 from django.views.generic import ListView, DetailView
 
 from ..models.models_transfer import School
+from ..models.model_major import Major
 # Create your views here.
 
+class HomeListView(ListView):
+    model = School
+    template_name = 'home.html'
 
 class SchoolListView(ListView):
     model = School
-    template_name = 'home.html'
+    template_name = 'school_detail.html'
+
+class MajorListView(ListView):
+    model = Major
+    template_name = 'major_detail.html'
