245f8025c9445f71cd2fb67a086c097317a51634
cmeehan340 <c.meehan340@gmail.com>
2020-04-07 08:34:57 -0400

    registered new data models, added links into navbar

--- transfer/admin.py
+++ transfer/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from .models.models_transfer import School, Course, MajorRequirment
+from .models.model_approver import Approver
+from .models.model_major import Major
 
 # Register your models here.
 admin.site.register(School)
 admin.site.register(Course)
 admin.site.register(MajorRequirment)
+admin.site.register(Approver)
+admin.site.register(Major)
--- transfer/models/model_major.py
+++ transfer/models/model_major.py
@@ -2,11 +2,7 @@ from django.db import models
 
 
 class Major(models.Model):
-    major_id = models.CharField(max_length=30, blank=False)
-    major_name = models.CharField(max_length=30, blank=False)
-
-    class Meta:
-        abstract = True
+    major_name = models.CharField(max_length=30, blank=False, unique=True)
 
     def __str__(self):
-        return self.major_id
+        return self.major_name
