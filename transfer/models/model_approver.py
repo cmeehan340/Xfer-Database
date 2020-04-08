cc3e8a1ebbb64f0f909544579d610951f114445c
cmeehan340 <c.meehan340@gmail.com>
2020-04-08 10:41:02 -0400

    Implemented Majors View, and Course View, populate majors view with data

--- transfer/models/model_approver.py
+++ transfer/models/model_approver.py
@@ -4,6 +4,5 @@ from django.db import models
 class Approver(models.Model):
     approver_name = models.CharField(max_length=30, blank=False, unique=True)
 
-
     def __str__(self):
         return self.approver_name
--- transfer/urls.py
+++ transfer/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 
-from .views.views import SchoolListView, HomeListView, MajorListView
+from .views.views import (
+        SchoolListView,
+        HomeListView,
+        MajorListView,
+        CourseListView
+    )
 
 urlpatterns = [
     path('', HomeListView.as_view(), name='home'),
     path('school', SchoolListView.as_view(), name='school_detail'),
-    path('major', MajorListView.as_view(), name='major_detail')
+    path('major', MajorListView.as_view(), name='major_detail'),
+    path('course', CourseListView.as_view(), name='course_detail'),
 ]
