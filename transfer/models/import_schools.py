8386a0d22350209f3c85075d402a59fb6156d7d2
cmeehan340 <c.meehan340@gmail.com>
2020-04-10 15:22:39 -0400

    Build import_schools script, which grabs all the school names and adds them into the database'

--- transfer/models/import_schools.py
+++ transfer/models/import_schools.py
@@ -2,15 +2,24 @@ import openpyxl
 import sqlite3
 from sqlite3 import Error
 
+path = '../data/'
+majors = ['Biological Sciences', 'Business', 'Communication Arts',
+            'Computer Information Systems (1)', 'Computer Science (1)',
+            'EET w_ CT Option (1)', 'Electrical Engineering Technology (1)',
+            'English (1)', 'English Teaching (1)', 'History (1)',
+            'Mechanical Engineering Technology (1)', 'Politics and Society (1)',
+            'Psychology (1)', 'Sign Language Interpretation (1)'
+        ]
+ext = '.xlsx'
+db_file = '../../db.sqlite3'
 
 
-def getSchools(sheet):
-    school_list = []
+
+def addSheetToList(sheet, school_list):
     for row_idx in range(sheet.min_row+1, sheet.max_row + 1):
         cell_content = sheet.cell(row=row_idx, column=1).value
         if cell_content not in school_list:
             if cell_content != None:
-                if cell_content not in ['NHTI', 'Colby Sawyer', 'UNH Durham']:
                     school_list.append(cell_content)
     return school_list
 
@@ -30,12 +39,12 @@ def create_school(conn, school):
     return cur.lastrowid
 
 if __name__ == '__main__':
-    file = '../data/Business.xlsx'
-    db_file = '../../db.sqlite3'
-    wb = openpyxl.load_workbook(file)
-
-    sheet = wb.active
-    school_list = getSchools(sheet)
+    school_list = []
+    for m in majors:
+        file = path + m + ext
+        wb = openpyxl.load_workbook(file)
+        sheet = wb.active
+        school_list = addSheetToList(sheet, school_list)
     print(school_list)
     conn = create_connection(db_file)
     with conn:
