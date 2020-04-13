76933c32a1cffdb245c6eb696283b8903e240fd5
cmeehan340 <c.meehan340@gmail.com>
2020-04-13 16:13:05 -0400

    Built dropdowns for all of the schools and majors, setup courses page

--- transfer/urls.py
+++ transfer/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from .views.course_view import CourseListView
 from .views.home_view import HomeListView
-from .views.major_view import MajorListView
+from .views.major_view import MajorListView, MajorDetailView
 from .views.school_view import SchoolListView
 
 
 urlpatterns = [
     path('', HomeListView.as_view(), name='home'),
     path('school', SchoolListView.as_view(), name='school_detail'),
-    path('major', MajorListView.as_view(), name='major_detail'),
+    path('major', MajorListView.as_view(), name='majors'),
     path('course', CourseListView.as_view(), name='course_detail'),
+    path('major/<int:pk>/', MajorDetailView.as_view(), name='major_detail'),
 ]
--- transfer/views/major_view.py
+++ transfer/views/major_view.py
@@ -4,12 +4,20 @@ Chris Meehan
 4/13/2020
 '''
 from ..models.model_major import Major
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 
 class MajorListView(ListView):
     '''
     This generates the view for majors.
+    '''
+    model = Major
+    template_name = 'majors.html'
+
+
+class MajorDetailView(DetailView):
+    '''
+    This will be the view for each major's specific page
     '''
     model = Major
     template_name = 'major_detail.html'
