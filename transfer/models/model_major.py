8c13de81dbbd1d7ee3c59b2bace15042a2d379b4
Chris <c.meehan340@gmail.com>
2020-05-06 19:01:01 -0400

    Increased max length of models due to error

--- transfer/models/model_major.py
+++ transfer/models/model_major.py
@@ -11,7 +11,7 @@ class Major(models.Model):
     '''
     This model is build on the given model for Majors.
     '''
-    major_name = models.CharField(max_length=30, blank=False, unique=True)
+    major_name = models.CharField(max_length=100, blank=False, unique=True)
 
     def __str__(self):
         return self.major_name
